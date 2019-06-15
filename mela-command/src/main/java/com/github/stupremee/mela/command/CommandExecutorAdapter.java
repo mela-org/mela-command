3e93fa39e18082deeba09348bcfaf7689f4555b4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-06-15 15:55:39 +0200

    Add todo

--- mela-command/src/main/java/com/github/stupremee/mela/command/CommandExecutorAdapter.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/CommandExecutorAdapter.java
@@ -6,7 +6,14 @@ import com.sk89q.intake.dispatcher.Dispatcher;
 /**
  * @author Johnny_JayJay (https://www.github.com/JohnnyJayJay)
  */
+// TODO: 15.06.2019 rethink CommandExecutor approach - where to put help & error response, prefix
 public abstract class CommandExecutorAdapter implements CommandExecutor {
+
+  private final Dispatcher dispatcher;
+
+  protected CommandExecutorAdapter(Dispatcher dispatcher) {
+    this.dispatcher = dispatcher;
+  }
 
   @Override
   public final void execute(Dispatcher dispatcher, String command, Namespace namespace) {
