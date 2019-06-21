0114489ccae6112d9276ba30d61f48768949fe93
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-06-21 10:54:41 +0200

    Add AbstractCommandModule again

---
+++ mela-command/src/main/java/com/github/stupremee/mela/command/AbstractCommandModule.java
@@ -0,0 +1,15 @@
+package com.github.stupremee.mela.command;
+
+import com.github.stupremee.mela.command.binder.CommandBinder;
+import com.google.inject.AbstractModule;
+
+/**
+ * @author Johnny_JayJay (https://www.github.com/JohnnyJayJay)
+ */
+public class AbstractCommandModule extends AbstractModule {
+
+  protected CommandBinder commands() {
+    return null;
+  }
+
+}
