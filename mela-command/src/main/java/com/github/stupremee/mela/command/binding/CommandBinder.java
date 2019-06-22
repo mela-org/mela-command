c9c510bbc0e9efd074a44111f0db1232e89a1171
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-06-22 17:59:41 +0200

    Add binder parameter to CommandBinder factory method

--- mela-command/src/main/java/com/github/stupremee/mela/command/binding/CommandBinder.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/binding/CommandBinder.java
@@ -1,10 +1,12 @@
 package com.github.stupremee.mela.command.binding;
 
+import com.google.inject.Binder;
+
 public interface CommandBinder {
 
   CommandBindingNode parentNode();
 
-  static CommandBinder create() {
+  static CommandBinder create(Binder binder) {
     return null;
   }
 }
