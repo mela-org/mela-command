1651c1dd64433c3a66247296b04840c9fa5bc6b4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-06-22 11:45:57 +0200

    Remove CommandNode accessors from API

--- mela-command/src/main/java/com/github/stupremee/mela/command/binder/CommandNode.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/binder/CommandNode.java
@@ -1,9 +1,6 @@
 package com.github.stupremee.mela.command.binder;
 
 import java.lang.annotation.Annotation;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public interface CommandNode {
 
@@ -18,9 +15,5 @@ public interface CommandNode {
   <T extends Throwable> ExceptionBindingBuilder<T> handle(Class<T> exceptionType);
 
   <T> ParameterBindingBuilder<T> bindParameter(Class<T> parameterType);
-
-  List<String> aliases();
-
-  Set<CommandNode> children();
 
 }
