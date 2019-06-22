6945b42d329ce51a43f1cdd953fda042386bfc9f
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-06-22 15:49:39 +0200

    Add retention & target to annotations

--- mela-command/src/main/java/com/github/stupremee/mela/command/CommandObjects.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/CommandObjects.java
@@ -1,7 +1,14 @@
 package com.github.stupremee.mela.command;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author Johnny_JayJay (https://www.github.com/JohnnyJayJay)
  */
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CommandObjects {
 }
