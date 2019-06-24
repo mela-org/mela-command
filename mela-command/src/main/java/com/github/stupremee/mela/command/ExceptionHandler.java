6ce4a8eca2b1ec9302aaeb4e6b2eb9eb0115047d
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-06-24 22:23:05 +0200

    Add placeholder methods for later ExceptionHandler, Interceptor and ArgumentMapper definitions

--- mela-command/src/main/java/com/github/stupremee/mela/command/ExceptionHandler.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/ExceptionHandler.java
@@ -2,6 +2,6 @@ package com.github.stupremee.mela.command;
 
 public interface ExceptionHandler<T extends Throwable> {
 
-
+  void handle(Throwable exception, CommandContext context); // TODO: 24.06.2019 replace with actual logic
 
 }
--- mela-command/src/main/java/com/github/stupremee/mela/command/Interceptor.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/Interceptor.java
@@ -4,4 +4,5 @@ import java.lang.annotation.Annotation;
 
 public interface Interceptor<T extends Annotation> {
 
+  boolean intercept(CommandContext context) throws Exception; // TODO: 24.06.2019 replace with actual logic
 }
