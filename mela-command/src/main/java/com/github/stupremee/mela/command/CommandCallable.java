80271ab8c89e070974cdaea091a18da35f4429b1
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-07-03 12:21:33 +0200

    Add NonNull and CheckReturnValue annotations to CommandCallable

--- mela-command/src/main/java/com/github/stupremee/mela/command/CommandCallable.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/CommandCallable.java
@@ -3,6 +3,8 @@ package com.github.stupremee.mela.command;
 import com.github.stupremee.mela.command.inject.DefaultCallableProvider;
 import com.google.inject.ProvidedBy;
 
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
 import java.util.Optional;
 import java.util.Set;
 
@@ -11,10 +13,14 @@ public interface CommandCallable {
 
   boolean call(String arguments, CommandContext context);
 
+  @Nonnull
+  @CheckReturnValue
   Selection selectChild(String arguments);
 
+  @Nonnull
   Set<CommandCallable> getChildren();
 
+  @Nonnull
   Set<String> getAliases();
 
   interface Selection {
