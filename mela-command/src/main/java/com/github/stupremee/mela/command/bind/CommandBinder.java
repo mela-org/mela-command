f7915bb1e5bf2dbd784c59c4c34e1189c4d33688
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-07-06 13:29:02 +0200

    Move null check in CommandBinder one layer down

--- mela-command/src/main/java/com/github/stupremee/mela/command/bind/CommandBinder.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/bind/CommandBinder.java
@@ -14,6 +14,6 @@ public interface CommandBinder {
 
   @Nonnull
   static CommandBinder create(@Nonnull Binder binder) {
-    return new InternalCommandBinder(checkNotNull(binder));
+    return new InternalCommandBinder(binder);
   }
 }
--- mela-command/src/main/java/com/github/stupremee/mela/command/bind/internal/InternalCommandBinder.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/bind/internal/InternalCommandBinder.java
@@ -2,9 +2,12 @@ package com.github.stupremee.mela.command.bind.internal;
 
 import com.github.stupremee.mela.command.bind.CommandBinder;
 import com.github.stupremee.mela.command.bind.CommandBindingNode;
+import com.google.common.base.Preconditions;
 import com.google.inject.Binder;
 
 import javax.annotation.Nonnull;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * @author Johnny_JayJay (https://www.github.com/JohnnyJayJay)
@@ -14,7 +17,9 @@ public final class InternalCommandBinder implements CommandBinder {
   private final CommandBindingNode root;
 
   public InternalCommandBinder(@Nonnull Binder binder) {
-    this.root = new InternalCommandBindingNode(new CommandMultibinder(binder));
+    this.root = new InternalCommandBindingNode(
+        new CommandMultibinder(checkNotNull(binder))
+    );
   }
 
   @Nonnull
