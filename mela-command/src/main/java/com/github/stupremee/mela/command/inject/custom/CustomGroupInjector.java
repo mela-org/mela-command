adfd10174f70f60c8025193d4970d8867702841f
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-07-09 12:24:20 +0200

    Remove GroupFinder

--- mela-command/src/main/java/com/github/stupremee/mela/command/inject/custom/CustomGroupInjector.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/inject/custom/CustomGroupInjector.java
@@ -1,7 +1,6 @@
 package com.github.stupremee.mela.command.inject.custom;
 
 import com.github.stupremee.mela.command.CommandGroup;
-import com.github.stupremee.mela.command.GroupFinder;
 import com.google.inject.MembersInjector;
 
 import java.lang.reflect.Field;
