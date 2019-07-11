bfefebe9ef2f29d9f1f1eec750e682837df0cf80
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-07-11 23:54:34 +0200

    Implement toString(), equals() and hashcode() for ImmutableGroup

--- mela-command/src/main/java/com/github/stupremee/mela/command/bind/tree/ImmutableGroup.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/bind/tree/ImmutableGroup.java
@@ -5,20 +5,21 @@ import com.google.common.collect.Sets;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * @author Johnny_JayJay (https://www.github.com/JohnnyJayJay)
  */
 public final class ImmutableGroup implements CommandGroup {
 
-  private final CommandGroup parent;
+  private final ImmutableGroup parent;
   private final Set<String> names;
   private final Set<CommandCallable> commands;
 
   private Set<CommandGroup> children;
 
-  private ImmutableGroup(CommandGroup parent, Set<String> names, Set<CommandCallable> commands) {
+  private ImmutableGroup(ImmutableGroup parent, Set<String> names, Set<CommandCallable> commands) {
     this.parent = parent;
     this.names = Set.copyOf(names);
     this.commands = Set.copyOf(commands);
@@ -48,7 +49,37 @@ public final class ImmutableGroup implements CommandGroup {
     return commands;
   }
 
-  private void setChildren(Set<CommandGroup> children) {
+  @Override
+  public String toString() {
+    StringBuilder pathBuilder = new StringBuilder();
+    ImmutableGroup group = this;
+    while (group != null) {
+      pathBuilder.insert(0, group.names);
+      if (group.parent != null)
+        pathBuilder.insert(0, " - ");
+      group = group.parent;
+    }
+    return pathBuilder.toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    ImmutableGroup that = (ImmutableGroup) o;
+    return Objects.equals(names, that.names) &&
+        Objects.equals(commands, that.commands) &&
+        Objects.equals(children, that.children);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(names, commands, children);
+  }
+
+  private void setChildren(Set<ImmutableGroup> children) {
     this.children = Set.copyOf(children);
   }
 
@@ -58,8 +89,8 @@ public final class ImmutableGroup implements CommandGroup {
     return copy;
   }
 
-  private static Set<CommandGroup> deepChildrenCopy(CommandGroup template, CommandGroup current) {
-    Set<CommandGroup> children = Sets.newHashSet();
+  private static Set<ImmutableGroup> deepChildrenCopy(CommandGroup template, ImmutableGroup current) {
+    Set<ImmutableGroup> children = Sets.newHashSet();
     for (CommandGroup child : template.getChildren()) {
       ImmutableGroup next = new ImmutableGroup(current, child.getNames(), child.getCommands());
       next.setChildren(deepChildrenCopy(child, next));
