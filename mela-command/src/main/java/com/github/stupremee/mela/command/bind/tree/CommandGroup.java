90335963ca9e31652aef1409d0a754bfdc3a12ef
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-07-11 23:30:49 +0200

    Add implementations for getPrimaryName() and matches() in CommandGroup again

--- mela-command/src/main/java/com/github/stupremee/mela/command/bind/tree/CommandGroup.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/bind/tree/CommandGroup.java
@@ -2,17 +2,23 @@ package com.github.stupremee.mela.command.bind.tree;
 
 import com.github.stupremee.mela.command.CommandCallable;
 import com.github.stupremee.mela.command.inject.RootGroupProvider;
+import com.google.common.base.Preconditions;
 import com.google.inject.ProvidedBy;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Set;
+import java.util.regex.Pattern;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * @author Johnny_JayJay (https://www.github.com/JohnnyJayJay)
  */
 @ProvidedBy(RootGroupProvider.class)
 public interface CommandGroup {
+
+  Pattern SPLIT_PATTERN = Pattern.compile("\\s+");
 
   @Nullable
   CommandGroup getParent();
@@ -27,11 +33,19 @@ public interface CommandGroup {
   Set<CommandCallable> getCommands();
 
   default boolean matches(String descriptor) {
-    return false;
+    String[] parts = SPLIT_PATTERN.split(checkNotNull(descriptor));
+    CommandGroup current = this;
+    for (int i = parts.length - 1; i >= 0; i--) {
+      if (current == null || !current.getNames().contains(parts[i]))
+        return false;
+      current = current.getParent();
+    }
+    return true;
   }
 
   default String getPrimaryName() {
-    return null;
+    Set<String> names = getNames();
+    return names.isEmpty() ? null : names.iterator().next();
   }
 
 }
