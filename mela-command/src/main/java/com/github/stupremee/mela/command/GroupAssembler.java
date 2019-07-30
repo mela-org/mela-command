cfb10b9c4a5ab96186ad9e62e3de2f2108568cfc
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-07-30 14:35:26 +0200

    Remove erroneous filter operation

--- mela-command/src/main/java/com/github/stupremee/mela/command/GroupAssembler.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/GroupAssembler.java
@@ -33,7 +33,6 @@ public interface GroupAssembler<T> {
 
   private static Set<CommandCallable> shallowCompile(CommandCompiler compiler, UncompiledGroup group) {
     return group.getUncompiledCommands().stream()
-        .filter((command) -> !(command instanceof CommandCallable))
         .map((command) -> compiler.compile(command, group.getBindings()))
         .collect(Sets::newHashSet, Set::addAll, Set::addAll);
   }
