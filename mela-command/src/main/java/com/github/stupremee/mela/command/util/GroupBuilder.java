8bc6bec44d7176e8351bac43f7c50a6d4d1b161e
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-09-07 20:46:51 +0200

    Decouple GroupBuilder from bindings

--- mela-command/src/main/java/com/github/stupremee/mela/command/util/GroupBuilder.java
+++ mela-command/src/main/java/com/github/stupremee/mela/command/util/GroupBuilder.java
@@ -1,25 +1,19 @@
 package com.github.stupremee.mela.command.util;
 
-import com.github.stupremee.mela.command.*;
-import com.github.stupremee.mela.command.bind.CommandBindings;
+import com.github.stupremee.mela.command.CommandGroup;
+import com.github.stupremee.mela.command.GroupAssembler;
+import com.github.stupremee.mela.command.ImmutableGroup;
 import com.github.stupremee.mela.command.compile.CommandCompiler;
 import com.github.stupremee.mela.command.compile.IdentityCompiler;
 import com.github.stupremee.mela.command.compile.UncompiledGroup;
-import com.github.stupremee.mela.command.handle.ExceptionHandler;
-import com.github.stupremee.mela.command.intercept.CommandInterceptor;
-import com.github.stupremee.mela.command.map.ArgumentMapper;
-import com.google.inject.Key;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.lang.annotation.Annotation;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
 /**
@@ -44,7 +38,7 @@ public final class GroupBuilder {
   }
 
   @Nonnull
-  public GroupBuilder group(Set<String> names) {
+  public GroupBuilder group(@Nonnull Set<String> names) {
     MutableGroup child = new MutableGroup(current, names);
     current.children.add(child);
     current = child;
@@ -54,27 +48,6 @@ public final class GroupBuilder {
   @Nonnull
   public GroupBuilder withCommand(Object command) {
     current.commands.add(command);
-    return this;
-  }
-
-  @Nonnull
-  public <T extends Annotation> GroupBuilder withInterceptor(@Nonnull Class<T> annotationType,
-                                                             @Nullable CommandInterceptor<T> interceptor) {
-    current.bindings.addInterceptor(annotationType, interceptor);
-    return this;
-  }
-
-  @Nonnull
-  public <T extends Throwable> GroupBuilder withHandler(@Nonnull Class<T> exceptionType,
-                                                        @Nullable ExceptionHandler<T> handler) {
-    current.bindings.addHandler(exceptionType, handler);
-    return this;
-  }
-
-  @Nonnull
-  public <T> GroupBuilder withMapper(@Nonnull Key<T> key,
-                                     @Nullable ArgumentMapper<T> mapper) {
-    current.bindings.addMapper(key, mapper);
     return this;
   }
 
@@ -111,16 +84,12 @@ public final class GroupBuilder {
     private final Set<String> names;
     private final Set<MutableGroup> children;
     private final Set<Object> commands;
-    private final MutableBindings bindings;
 
     private MutableGroup(MutableGroup parent, Set<String> names) {
       this.parent = parent;
       this.names = Set.copyOf(names);
       children = new HashSet<>();
       commands = new HashSet<>();
-      this.bindings = parent == null
-          ? new MutableBindings(null)
-          : parent.bindings;
     }
 
     @Nonnull
@@ -145,25 +114,6 @@ public final class GroupBuilder {
     @Override
     public Set<String> getNames() {
       return names;
-    }
-  }
-
-  private static final class MutableBindings extends CommandBindings {
-
-    MutableBindings(MutableBindings parent) {
-      super(parent);
-    }
-
-    <T extends Annotation> void addInterceptor(Class<T> annotationType, CommandInterceptor<T> interceptor) {
-      interceptors.put(checkNotNull(annotationType), () -> interceptor);
-    }
-
-    <T extends Throwable> void addHandler(Class<T> exceptionType, ExceptionHandler<T> handler) {
-      handlers.put(checkNotNull(exceptionType), () -> handler);
-    }
-
-    <T> void addMapper(Key<T> key, ArgumentMapper<T> mapper) {
-      mappers.put(checkNotNull(key), () -> mapper);
     }
   }
 }
