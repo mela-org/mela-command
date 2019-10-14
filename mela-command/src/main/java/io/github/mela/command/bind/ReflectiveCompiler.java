e66b05889284ea5ce53b93d813b6034c095049c4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-10-14 20:22:00 +0200

    Catch compilation exceptions

--- mela-command/src/main/java/io/github/mela/command/bind/ReflectiveCompiler.java
+++ mela-command/src/main/java/io/github/mela/command/bind/ReflectiveCompiler.java
@@ -5,6 +5,7 @@ import io.github.mela.command.compile.CommandCompiler;
 import io.github.mela.command.core.CommandCallable;
 
 import javax.annotation.Nonnull;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -27,7 +28,17 @@ public class ReflectiveCompiler implements CommandCompiler {
   public Set<? extends CommandCallable> compile(@Nonnull Object command) {
     return Arrays.stream(command.getClass().getMethods())
         .filter((method) -> method.isAnnotationPresent(Command.class))
-        .map((method) -> ReflectiveCallable.from(command, method, bindings))
+        .map((method) -> compile(command, method))
         .collect(Collectors.toUnmodifiableSet());
+  }
+
+  private CommandCallable compile(Object command, Method method) {
+    try {
+      return ReflectiveCallable.from(command, method, bindings);
+    } catch (NoSuchMethodException e) {
+      throw new AssertionError("A method directly taken from a Class cannot be found anymore. Huh?", e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException("Command methods must be public", e);
+    }
   }
 }
