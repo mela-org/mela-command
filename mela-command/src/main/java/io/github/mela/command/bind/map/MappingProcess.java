2070c994b48d2b7545af59b05a444a230ccee066
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-03-20 14:58:53 +0100

    MAke access to MappingProcess creation package-private, make MappingProcessor class final

--- mela-command/src/main/java/io/github/mela/command/bind/map/MappingProcess.java
+++ mela-command/src/main/java/io/github/mela/command/bind/map/MappingProcess.java
@@ -32,8 +32,9 @@ public final class MappingProcess {
     this.arguments = arguments;
   }
 
-  public static MappingProcess create(@Nonnull TargetType targetType, Arguments arguments) {
+  static MappingProcess create(@Nonnull TargetType targetType, @Nonnull Arguments arguments) {
     checkNotNull(targetType);
+    checkNotNull(arguments);
     return new MappingProcess(targetType, arguments);
   }
 
