efe506f8666595ad1bc350243f3b2839853ad7fb
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-03-21 14:21:08 +0100

    Don't use javax.inject in tests anymore

--- src/test/java/io/github/mela/command/bind/parameter/CommandParameterTest.java
+++ src/test/java/io/github/mela/command/bind/parameter/CommandParameterTest.java
@@ -2,7 +2,6 @@ package io.github.mela.command.bind.parameter;
 
 import org.junit.jupiter.api.Test;
 
-import javax.inject.Named;
 import java.lang.reflect.Parameter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,18 +9,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CommandParameterTest {
 
   private static final String DESCRIPTION = "ABCDEFGH";
-  private static final String NAME = "IJKLMNOP";
 
   @Test
   void testParameterParsing() throws NoSuchMethodException {
     Parameter parameter = this.getClass().getMethod("method", String.class).getParameters()[0];
     CommandParameter commandParameter = CommandParameter.of(parameter);
-    assertEquals(NAME, commandParameter.getName(), "Name was not recognised");
+    assertEquals("object", commandParameter.getName(), "Name was not recognised");
     assertEquals(String.class, commandParameter.getTargetType().getType(),
         "Type was not recognised");
     assertEquals(DESCRIPTION, commandParameter.getDescription(), "Description was not recognised");
   }
 
-  public void method(@Named(NAME) @Description(DESCRIPTION) String object) {}
+  public void method(@Description(DESCRIPTION) String object) {}
 
 }
